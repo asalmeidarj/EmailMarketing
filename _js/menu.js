0f6020b43b488dab72328baa3a362754ffaa0a1d
aslameidarj <asalmeidarj@gmail.com>
2021-07-03 21:36:55 -0300

    Cabeçalho Hambúrguer...

---
+++ _js/menu.js
@@ -0,0 +1,11 @@
+
+const btnMobile = document.getElementById('btn-menu'); 
+
+function toggleMenu() {
+	const nav = document.getElementById('nav');
+	nav.classList.toggle('active');
+}
+
+
+btnMobile.addEventListener('click', toggleMenu);
+
